dafd64aa2d604d80ddea1014773ff4b4a63c85c3
Federico Sassone <fsassone@ws7.labo1>
2016-09-07 20:22:53 -0300

    pixelar_c funcando

--- codigo/filtros/rotar_canales_c.c
+++ codigo/filtros/rotar_canales_c.c
@@ -6,22 +6,6 @@ void rotar_c (unsigned char *src, unsigned char *dst, int cols, int filas, int s
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
 
 	//COMPLETAR
-	
-	/*	Funcion que vamos a usar siempre para recorrer en c:
-
-		for (int f = 0; f < filas; f++) {
-			for (int c = 0; c < cols; c++) {
-				bgra_t *p_d = (bgra_t*) &dst_matrix[f][c * 4];
-				bgra_t *p_s = (bgra_t*) &src_matrix[f][c * 4];
-
-				p_d->b = p_s->b;
-				p_d->g = p_s->g;
-				p_d->r = p_s->r;
-				p_d->a = p_s->a;
-			}
-		}			
-	*/
-
 		for (int f = 0; f < filas; f++) {
 			for (int c = 0; c < cols; c++) {
 				bgra_t *p_d = (bgra_t*) &dst_matrix[f][c * 4];
