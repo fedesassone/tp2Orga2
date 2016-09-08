d23198685ba3b18c9e1d0cc90fbc4b247ce7aa3b
Federico Sassone <fsassone@ws7.labo1>
2016-09-08 09:50:53 -0300

    combinar_c done

--- codigo/filtros/colorizar_c.c
+++ codigo/filtros/colorizar_c.c
@@ -16,4 +16,19 @@ void colorizar_c (
 
 	// TODO: Implementar
 
+
+/*	Funcion que vamos a usar siempre para recorrer en c:
+
+		for (int f = 0; f < filas; f++) {
+			for (int c = 0; c < cols; c++) {
+				bgra_t *p_d = (bgra_t*) &dst_matrix[f][c * 4];
+				bgra_t *p_s = (bgra_t*) &src_matrix[f][c * 4];
+
+				p_d->b = p_s->b;
+				p_d->g = p_s->g;
+				p_d->r = p_s->r;
+				p_d->a = p_s->a;
+			}
+		}			
+*/
 }
