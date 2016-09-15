f7c20719a4d8017d4f229b9e584f28b77a2ef469
Federico Sassone <fsassone@ws6.labo2>
2016-09-15 11:23:31 -0300

    arreglados filtros C

--- codigo/filtros/pixelar_c.c
+++ codigo/filtros/pixelar_c.c
@@ -14,25 +14,25 @@ void pixelar_c (
 
 	//COMPLETAR
 	
-		for (int f = 0; f < filas-1; f=f+2) {
-			for (int c = 0; c < cols-1; c=c+2) {
-				bgra_t *p_d = (bgra_t*) &dst_matrix[f][c * 4];
+		for (int f = 0; f < filas; f=f+2) {
+			for (int c = 0; c < cols; c=c+2) {
+				//bgra_t *p_d = (bgra_t*) &dst_matrix[f][c * 4];
 
-				bgra_t *p_d0 = (bgra_t*) &dst_matrix[f    ][  c   * 4];
-				bgra_t *p_d1 = (bgra_t*) &dst_matrix[f + 1][(c+1) * 4];
-				bgra_t *p_d2 = (bgra_t*) &dst_matrix[f    ][  c   * 4];
-				bgra_t *p_d3 = (bgra_t*) &dst_matrix[f + 1][(c+1) * 4];
+				bgra_t *p_d0 = (bgra_t*) &dst_matrix[ f ][  c   * 4]; //primeto
+				bgra_t *p_d1 = (bgra_t*) &dst_matrix[ f ][(c+1) * 4]; //segundo
+				bgra_t *p_d2 = (bgra_t*) &dst_matrix[f+1][  c   * 4]; //tercer
+				bgra_t *p_d3 = (bgra_t*) &dst_matrix[f+1][(c+1) * 4]; //cuarto
 
 
-				bgra_t *p_s0 = (bgra_t*) &src_matrix[f    ][  c   * 4];
-				bgra_t *p_s1 = (bgra_t*) &src_matrix[f + 1][(c+1) * 4];
-				bgra_t *p_s2 = (bgra_t*) &src_matrix[f    ][  c   * 4];
-				bgra_t *p_s3 = (bgra_t*) &src_matrix[f + 1][(c+1) * 4];
+				bgra_t *p_s0 = (bgra_t*) &src_matrix[ f ][  c   * 4];
+				bgra_t *p_s1 = (bgra_t*) &src_matrix[ f ][(c+1) * 4];
+				bgra_t *p_s2 = (bgra_t*) &src_matrix[f+1][ c    * 4];
+				bgra_t *p_s3 = (bgra_t*) &src_matrix[f+1][(c+1) * 4];
 
 				//promedios
-				unsigned char bi = (p_s0->b + p_s1->b + p_s2->b + p_s3->b)/4;
-				unsigned char gi = (p_s0->g + p_s1->g + p_s2->g + p_s3->g)/4;
-				unsigned char ri = (p_s0->r + p_s1->r + p_s2->r + p_s3->r)/4;
+				unsigned char bi = ((p_s0->b + p_s1->b + p_s2->b + p_s3->b)/4);
+				unsigned char gi = ((p_s0->g + p_s1->g + p_s2->g + p_s3->g)/4);
+				unsigned char ri = ((p_s0->r + p_s1->r + p_s2->r + p_s3->r)/4);
 
 				p_d0->b = bi;
 				p_d0->g = gi;
--- codigo/filtros/smalltiles_c.c
+++ codigo/filtros/smalltiles_c.c
@@ -2,25 +2,40 @@
 #include "../tp2.h"
 
 void smalltiles_c (unsigned char *src, unsigned char *dst, int cols, int filas, int src_row_size, int dst_row_size) {
-	//COMPLETAR
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
-	
-	for (int f = 0; f < filas/2; f++) {
-		for (int c = 0; c < cols; c++) {
-			bgra_t *p_d0 = (bgra_t*) &dst_matrix[f][c * 4];
-			bgra_t *p_d1 = (bgra_t*) &dst_matrix[filas/2 + f][c * 4];
-            bgra_t *p_s = (bgra_t*) &src_matrix[2*f][2*c * 4];
+		
+		//tercer // cuarto
+		//primer // segundo
 
+	for (int f = 0; f < filas/2; f++) {
+		for (int c = 0; c < cols/2; c++) {
+			bgra_t *p_d0 = (bgra_t*) &dst_matrix[f][c * 4]; 							//primer
+			bgra_t *p_d1 = (bgra_t*) &dst_matrix[f][((cols*4)/2) + c * 4];				//segundo
+			bgra_t *p_d2 = (bgra_t*) &dst_matrix[(filas/2) + f][c * 4];  				//tercer
+			bgra_t *p_d3 = (bgra_t*) &dst_matrix[(filas/2) + f][((cols*4)/2) + c * 4];	//cuarto
+
+            bgra_t *p_s = (bgra_t*) &src_matrix[2*f][2*c * 4];
+            
 			p_d0->b = p_s->b;
 			p_d0->g = p_s->g;
 			p_d0->r = p_s->r;
-			p_d0->a = p_s->a;
+			p_d0->a = p_s->a;		
 		
 			p_d1->b = p_s->b;
 			p_d1->g = p_s->g;
 			p_d1->r = p_s->r;
 			p_d1->a = p_s->a;
+
+			p_d2->b = p_s->b;
+			p_d2->g = p_s->g;
+			p_d2->r = p_s->r;
+			p_d2->a = p_s->a;
+
+			p_d3->b = p_s->b;
+			p_d3->g = p_s->g;
+			p_d3->r = p_s->r;
+			p_d3->a = p_s->a;		
 		}
 	}
 }
