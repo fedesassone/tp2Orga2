5c6982b45f0528e8250519cac410a7fbfc2e4827
Luciano <luchosaenz@users.noreply.github.com>
2016-09-19 17:38:22 -0300

    Update pixelar_asm.asm

--- codigo/filtros/pixelar_asm.asm
+++ codigo/filtros/pixelar_asm.asm
@@ -14,10 +14,11 @@
 ; 	rcx = filas
 ; 	r8 = src_row_size
 ; 	r9 = dst_row_size
-
 extern pixelar_c
 
 global pixelar_asm
+section .data
+mask: dw 0x0400,0x0400,0x0400,0x0400
 
 section .text
 	
@@ -25,62 +26,63 @@ pixelar_asm:
 	;; TODO: Implementar
 	push rbp
 	mov rbp, rsp
-	sub rsp, 8
 	push rbx
+	push r12
+
 	pxor xmm7, xmm7
+	mov r12, rdx
 	mov rbx, rcx
+	shr rbx, 1
 ciclo_pixelar_alto:
-	mov rcx, rdx
+	mov rcx, r12
 	shr rcx, 2
 ciclo_pixelar_ancho:	
-	movdqu xmm1, [rdi] 		;xmm1 = |a3|a2|a1|a0|
-	movdqu xmm3, [rdi + r8]	;xmm3 = |b3|b2|b1|b0|
+	movdqu xmm1, [rdi] 		;xmm1 = |pa3|pa2|pa1|pa0|
+	movdqu xmm3, [rdi + r8]	;xmm3 = |pb3|pb2|pb1|pb0|
 	movdqu xmm2, xmm1		;xmm2 = xmm1
 	movdqu xmm4, xmm3		;xmm4 = xmm3
-	punpckhdq xmm1, xmm7	;xmm1 = |0|a3|0|a2|
-	punpckldq xmm2, xmm7	;xmm2 = |0|a1|0|a0|
-	punpckhdq xmm3, xmm7	;xmm3 = |0|b3|0|b2|
-	punpckldq xmm4, xmm7	;xmm4 = |0|b1|0|b0|
+	punpckhbw xmm1, xmm7	;xmm1 = |0|a127|..|0|a64|
+	punpcklbw xmm2, xmm7	;xmm2 = |0|a63|..|0|a0|
+	punpckhbw xmm3, xmm7	;xmm3 = |0|b127|..|0|b64|
+	punpcklbw xmm4, xmm7	;xmm4 = |0|b63|..|0|b0|
 
-	addss xmm1, xmm3		;xmm1 = |?|a3+b3|?|a2+b2|
+	addss xmm1, xmm3		;xmm1 = |?|a127+b127|..|?|a64+b64|
 	movdqu xmm3, xmm1		;xmm3 = xmm1
-	psrldq xmm1, 8			;xmm1 = |0|0|?|a3+b3|
-	addss xmm1, xmm3		;xmm1 = |?|a3+b3|?|a3+b3+a2+b2|
-	pslldq xmm1, 8			;xmm1 = |?|a3+b3+a2+b2|0|0|
-
-	addss xmm2, xmm4		;xmm2 = |?|a1+b1|?|a0+b0|
+	psrldq xmm1, 8			;xmm1 = |0|0|?|a127+b127..|
+	addps xmm1, xmm3		;xmm1 = |?|a127+b127..|?|a127+b127+..+a64+b64|
+	
+	addss xmm2, xmm4		;xmm2 = |?|a63+b63|..|?|a0+b0|
 	movdqu xmm4, xmm2		;xmm4 = xmm2
-	psrldq xmm2, 8			;xmm2 = |0|0|?|a1+b1|
-	addss xmm2, xmm4		;xmm1 = |?|a1+b1|?|a1+b1+a0+b0|
-	pslldq xmm2, 8			;xmm2 = |?|a1+b1+a0+b0|0|0|
-	psrldq xmm2, 8			;xmm2 = |0|0|?|a1+b1+a0+b0|
-
-	addss xmm1, xmm2
-	;xmm0 = |4|4|
-	;divpd xmm1, xmm0
-	;movdqu xmm2, xmm1
-	;pshrldq xmm1, 8
-	;movdqu xmm3, xmm1
-	;pshlldq xmm3, 4
-	;addss xmm1, xmm3
-
-	;movdqu xmm4, xmm2
-	;pshlldq xmm4, 4
-	;addss xmm2, xmm4
-	;movq [rsi], xmm1
-	;movq [rsi + 8], xmm2
-	;movq [rsi + r8], xmm1
-	;movq [rsi + r8 + 8], xmm2
+	psrldq xmm2, 8			;xmm2 = |0|0|?|a63+b63..|
+	addps xmm2, xmm4		;xmm1 = |?|a63+b63..|?|a63+b63+..+a0+b0|
+	
+	movdqu xmm0, [mask]
+	punpcklwd xmm1, xmm7
+	punpcklwd xmm2, xmm7
+	;divps xmm1, xmm0		;xmm1 = |r/4|g/4|b/4|a/4|
+	;divps xmm2, xmm0		;xmm2 = |r/4|g/4|b/4|a/4|
+	packusdw xmm1, xmm1
+	packusdw xmm2, xmm2
+	;pshufd xmm1,xmm1,44h
+	;pshufd xmm2,xmm2,44h
+	packuswb xmm2, xmm1 
+	movdqu [rsi], xmm2
+	movdqu [rsi + r8], xmm2
 	add rsi, 16
 	add rdi, 16
-	loop ciclo_pixelar_ancho
+	dec rcx
+	cmp rcx, 0
+	jne ciclo_pixelar_ancho
 	
 	dec rbx
 	cmp rbx, 0
 	je fin_pixelar
 	lea rdi, [rdi + r8]
 	lea rsi, [rsi + r8]
+	jmp ciclo_pixelar_alto
 fin_pixelar:
+	pop r12
+	pop rbx
 	pop rbp
 	ret
 
