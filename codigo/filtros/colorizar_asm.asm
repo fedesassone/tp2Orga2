ce39bfe5436e6bdbe1811fa76c0540c6ec0e9881
Federico Sassone <fsassone@ws8.labo5>
2016-10-12 20:30:50 -0300

    update colorizar v0.9

--- codigo/filtros/colorizar_asm.asm
+++ codigo/filtros/colorizar_asm.asm
@@ -23,6 +23,9 @@ DEFAULT REL
 
 section .text
 
+
+;;;; | B | G | R | A | ;;;;;
+
 global colorizar_asm
 colorizar_asm:
 
@@ -36,6 +39,10 @@ colorizar_asm:
 	 sub rsp, 8
 
 	 	movdqu xmm4, xmm0		; guardo alpha 
+		pshufd xmm4, xmm4, 0b 	; xmm4 = alpha | alpha | alpha | alpha
+		xor eax, eax
+		pinsrd xmm4, eax, 0b 	; xmm4 =  alpha | alpha | alpha | 0
+
 	 	mov r12, rsi 			; r12 = dst 
 	 	mov r13, rdi			; r13 = src 	 	
 ;busco cantidad de ciclos	 	
@@ -80,50 +87,54 @@ colorizar_asm:
 			punpcklbw xmm2, xmm0 	
 			punpcklwd xmm2, xmm0	; xmm2 = |maxb | maxg | maxr | tr | PIXEL 1 
 			
+			;seteos previos a comparaciones
+			
+			mov eax, -1 ;	rax = -1
+			mov ebx,  1	;	rbx =  1
+
 			;---- Pixel 2
 			; comparaciones 
 			pextrd edi, xmm1, 1 ; maxred 
 			pextrd esi, xmm1, 2 ; maxgreen
 			pextrd edx, xmm1, 3 ; maxblue 
 
-			mov rbx, 1b ;	rbx = 1
-			pshufd xmm4, xmm4, 0b 	; xmm4 = alpha | alpha | alpha | alpha
-			pinsrd xmm4, ebx, 0b 	; xmm4 =  alpha | alpha | alpha | 0
-			xor rax, rax
-			not rax ; eax = -1 
+;			pshufd xmm4, xmm4, 0b 	; xmm4 = alpha | alpha | alpha | alpha
+;			pinsrd xmm4, ebx, 0b 	; xmm4 =  alpha | alpha | alpha | 0
+;			xor rax, rax
+;			not rax ; eax = -1 
 			; ebx = alpha
 			pxor xmm5, xmm5  		; XMM5 = 0's  
-			cvtdq2ps xmm5, xmm5 
+			;cvtdq2ps xmm5, xmm5 
 			.fiR2:
 			;float fiR = ( (maxR >= maxG && maxR >= maxB) ? 1.0 + al: 1.0 - al )
 			cmp edi, esi
 			jl .elseFiR2
 			cmp edi, edx
 			jl .elseFiR2
-			pinsrd xmm5, ebx, 1b ; xmm5 = | 1  | 0's..
+			pinsrd xmm5, ebx, 1 ; xmm5 = | 1  | 0's..
 			jmp .fiG2
 			.elseFiR2:
-			pinsrd xmm5, eax, 1b ; xmm5 = | -1 | 0's.. 
+			pinsrd xmm5, eax, 1 ; xmm5 = | -1 | 0's.. 
 			.fiG2: 
 			;float fiG = ( (maxR <  maxG && maxG >= maxB) ? 1.0 + al: 1.0 - al )
-			cmp edi, edx
-			jge .elseFiG2
+			cmp esi, edi
+			jle .elseFiG2
 			cmp esi, edx
-			jge .elseFiG2
-			pinsrd xmm5, ebx, 10b; xmm5 = ? | 1 | 0's.. 
+			jl .elseFiG2
+			pinsrd xmm5, ebx, 2; xmm5 = ? | 1 | 0's.. 
 			jmp .fiB2
 			.elseFiG2:
-			pinsrd xmm5, eax, 10b; xmm5 = ? | -1 | 0's 
+			pinsrd xmm5, eax, 2; xmm5 = ? | -1 | 0's 
 			.fiB2:
  			;float fiB = ( (maxR <  maxB && maxG <  maxB) ? 1.0 + al: 1.0 - al )			
- 			cmp edi, edx
- 			jge .elseFiB2
- 			cmp esi, edx
- 			jge .elseFiB2
- 			pinsrd xmm5, ebx, 11b  ; xmm5 = ? | ? | 1 | 0	
+ 			cmp edx, edi
+ 			jle .elseFiB2
+ 			cmp edx, esi
+ 			jle .elseFiB2
+ 			pinsrd xmm5, ebx, 3  ; xmm5 = ? | ? | 1 | 0	
  			jmp .sigo2
  			.elseFiB2:
- 			pinsrd xmm5, eax, 11b ; xmm5 = ? | ? | -1 | 0 
+ 			pinsrd xmm5, eax, 3 ; xmm5 = ? | ? | -1 | 0 
  			.sigo2:
  			cvtdq2ps xmm5, xmm5 ; xmm5 = valores a sumar en floats 
  			addps xmm5, xmm4  	; xmm5 = fiB | fiG | fiR | 0  			DEL PIXEL 2
@@ -134,7 +145,7 @@ colorizar_asm:
 			pextrd esi, xmm2, 2 ; maxgreen
 			pextrd edx, xmm2, 3 ; maxblue 
 			pxor xmm6, xmm6
-			cvtdq2ps xmm6, xmm6
+			;cvtdq2ps xmm6, xmm6
 			
 			.fiR1:
 			;float fiR = ( (maxR >= maxG && maxR >= maxB) ? 1.0 + al: 1.0 - al )
@@ -142,35 +153,36 @@ colorizar_asm:
 			jl .elseFiR1
 			cmp edi, edx
 			jl .elseFiR1
-			pinsrd xmm6, ebx, 1b ; xmm5 = | 1  | 0's..
+			pinsrd xmm6, ebx, 1 ; 
 			jmp .fiG1
 			.elseFiR1:
-			pinsrd xmm6, eax, 1b ; xmm5 = | -1 | 0's.. 
+			pinsrd xmm6, eax, 1 ; 
 			.fiG1: 
 			;float fiG = ( (maxR <  maxG && maxG >= maxB) ? 1.0 + al: 1.0 - al )
-			cmp edi, edx
-			jge .elseFiG1
+			cmp esi, edi
+			jle .elseFiG1
 			cmp esi, edx
-			jge .elseFiG1
-			pinsrd xmm6, ebx, 10b; xmm5 = ? | 1 | 0's.. 
+			jl .elseFiG1
+			pinsrd xmm6, ebx, 2; xmm5 = ? | 1 | 0's.. 
 			jmp .fiB1
 			.elseFiG1:
-			pinsrd xmm6, eax, 10b; xmm5 = ? | -1 | 0's 
+			pinsrd xmm6, eax, 2; xmm5 = ? | -1 | 0's 
 			.fiB1:
  			;float fiB = ( (maxR <  maxB && maxG <  maxB) ? 1.0 + al: 1.0 - al )			
- 			cmp edi, edx
- 			jge .elseFiB1
- 			cmp esi, edx
- 			jge .elseFiB1
- 			pinsrd xmm6, ebx, 11b  ; xmm5 = ? | ? | 1 | 0	
+ 			cmp edx, edi
+ 			jle .elseFiB1
+ 			cmp edx, esi
+ 			jle .elseFiB1
+ 			pinsrd xmm6, ebx, 3  ; xmm5 = ? | ? | 1 | 0	
  			jmp .sigo1
  			.elseFiB1:
- 			pinsrd xmm6, eax, 11b ; xmm5 = ? | ? | -1 | 0 
+ 			pinsrd xmm6, eax, 3 ; xmm5 = ? | ? | -1 | 0 
  			.sigo1:
  			cvtdq2ps xmm6, xmm6 ; xmm6 = valores a sumar en floats 
  			addps xmm6, xmm4  	; xmm6 = fiB | fiG | fiR | 0  			DEL PIXEL 1
 
-
+ 			;xmm5 = pixel 2
+ 			;xmm6 = pixel 1 
 
  			pxor xmm7, xmm7
  			pxor xmm1, xmm1
@@ -193,9 +205,12 @@ colorizar_asm:
  			mulps xmm2, xmm5 		; xmm2 = |blue2*fiB2 | ..... | 0
  			mov eax, 255
  			pxor xmm0, xmm0
- 			pinsrd xmm0, eax, 1b
- 			pshufd xmm0, xmm0, 1010100b    
- 			cvtdq2ps xmm0, xmm0 	; xmm0 = 255 en float 4 veces
+ 			pinsrd xmm0, eax, 1
+ 			pinsrd xmm0, eax, 2
+ 			pinsrd xmm0, eax, 3
+ 			;pinsrd xmm0, eax, 1
+ 			;pshufd xmm0, xmm0, 1010100b    
+ 			cvtdq2ps xmm0, xmm0 	; xmm0 = 255 en float 3 veces y 0
  			movdqu xmm8, xmm0		; xmm8 = xmm0
  			movdqu xmm9, xmm0
 
@@ -204,35 +219,44 @@ colorizar_asm:
  			;pcmpeqd xmm0, xmm1		; xmm1 = (fiC * C) < 255? 255 ; 0)
  			;pcmpeqw xmm0, xmm0 		; xmm0 = 255 en entero donde hay que poner 0.5 + sat
  									; xmm9 = FF donde hay que poner 0.5 + sat en pixel 2
- 			movdqu xmm7, xmm0
- 			movdqu xmm10, xmm9 		
- 			pcmpeqw xmm10, xmm10 		; xmm10 = FF donde hay que poner 255 en float pixel 2 
- 			pcmpeqw xmm7, xmm7 		; xmm7 = 255 en entero donde hay que poner 255 en float  
+ 			movdqu xmm7, xmm0		;xmm7 = mascara ed menor o no a 255 edl pix 1
+ 			movdqu xmm10, xmm9 		;xmm10 = mascara ed menor o no a 255 edl pix 2
+ 			pcmpeqd xmm10, xmm10 	; xmm10 = FF donde hay que poner 255 en float pixel 2 
+ 			pcmpeqd xmm7, xmm7 		; xmm7  = FF en entero donde hay que poner 255 en float  
  			pand xmm1, xmm0 		; xmm1 = fiC * c donde tiene que haberlo
  			pand xmm2, xmm9 		; xmm2 = fiC * C donde tiene que haberlo pixel 2  
 
- 			pinsrd xmm5, ebx, 1b
- 			pshufd xmm5, xmm5, 01010100b 	; xmm5 = 1 | 1 | 1 | 0
- 			mov eax, 10b
- 			pinsrd xmm6, eax, 1b 
- 			pshufd xmm6, xmm6, 01010100b 	; xmm5 = 2 | 2 | 2 | 0
+pxor xmm5, xmm5 
+ 			pinsrd xmm5, ebx, 1
+ 			pinsrd xmm5, ebx, 2
+ 			pinsrd xmm5, ebx, 3
+
+ 			;pshufd xmm5, xmm5, 01010100b 	; xmm5 = 1 | 1 | 1 | 0
+pxor xmm6, xmm6 
+ 			mov eax, 2
+ 			pinsrd xmm6, eax, 1
+ 			pinsrd xmm6, eax, 2
+ 			pinsrd xmm6, eax, 3
+
+ 			;pshufd xmm6, xmm6, 01010100b 	; xmm5 = 2 | 2 | 2 | 0
  			cvtdq2ps xmm5, xmm5 			
  			cvtdq2ps xmm6, xmm6				;ambos en float
  			divps xmm5, xmm6 				; xmm5= 1/2 | 1/2 | 1/2 | basura 
- 			mov eax, 0b
- 			pinsrd xmm5, eax, 0b 			;  xmm5= 1/2 | 1/2 | 1/2 | 0
+ 			mov eax, 0
+ 			pinsrd xmm5, eax, 0 			;  xmm5= 1/2 | 1/2 | 1/2 | 0
  			movdqu xmm6, xmm5 
+ 			
  			pand xmm5, xmm0 				; xmm5 = 0,5 donde tiene que haberlo 
  			pand xmm6, xmm9					; pixel 2
  			addps xmm5, xmm1 				; 0,5 + fi*c donde tiene que haberlo 
  			addps xmm6, xmm2 				; pixel 2 
 
-
- 			pand xmm10, xmm8 				; 255 en float donde tiene que haber pix2
- 			por xmm6, xmm10 				;255 donde debe y o,5 + fi*c donde debe pixel2 
+;///chequear esto 
+; 			pand xmm10, xmm8 				; 255 en float donde tiene que haber pix2
+ ;			por xmm6, xmm10 				;255 donde debe y o,5 + fi*c donde debe pixel2 
  			;pxor xmm0, xmm0
- 			pand xmm7, xmm8 				; 255 en float donde tiene que haber 
- 			por xmm5, xmm7 					; 255 donde debe, y 0,5 +fi*c donde debe
+ ;			pand xmm7, xmm8 				; 255 en float donde tiene que haber 
+ ;			por xmm5, xmm7 					; 255 donde debe, y 0,5 +fi*c donde debe
  			; hasta acá esta todo en float 
  			;convierto a INT
 
